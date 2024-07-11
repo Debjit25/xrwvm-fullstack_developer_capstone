2a766fe177c4d291ece7a8470073dd180c07074f
Debjit Maji <debjitmaji123@gmail.com>
2024-07-11 16:00:23 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
-
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 function App() {
   return (
     <Routes>
